4c65f3cf7ae7dc62f34a1244d20f3d99e27eadac
coord.e <me@coord-e.com>
2020-11-03 14:18:49 +0000

    Add: Enable to invoke extension by name dynamically

--- app/App.hs
+++ app/App.hs
@@ -1,29 +1,73 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
-module App (App, runApp, handleError) where
+module App (App, runApp, parseProgram, typeProgram) where
 
 import Control.Monad.Except (ExceptT (..), runExceptT)
 import Control.Monad.IO.Class (MonadIO (..))
 import Control.Monad.Logger (LoggingT, MonadLogger, logErrorN, runStdoutLoggingT)
-import Prettyprinter (Doc, Pretty (..), defaultLayoutOptions, layoutPretty)
+import Data.List (find)
+import Data.Proxy (Proxy (..))
+import Data.Text (Text)
+import Language.Simple.Extension (Extension)
+import Language.Simple.Extension.SimpleUnification (SimpleUnification)
+import Language.Simple.Extension.TypeClass (TypeClass)
+import qualified Language.Simple.Parser as Parser (parseProgram)
+import Language.Simple.Syntax (Program)
+import qualified Language.Simple.Type as Type (typeProgram)
+import Prettyprinter (Doc, Pretty (..), defaultLayoutOptions, dquotes, layoutPretty, (<+>))
 import Prettyprinter.Render.Text (renderStrict)
 import System.Exit (exitFailure)
 
-newtype App a = App (ExceptT (Doc ()) (LoggingT IO) a)
+newtype App x a = App (ExceptT (Doc ()) (LoggingT IO) a)
   deriving (Functor, Applicative, Monad, MonadIO, MonadLogger)
 
-handleError :: Pretty e => ExceptT e App a -> App a
+handleError :: Pretty e => ExceptT e (App x) a -> App x a
 handleError a = do
   e <- runExceptT a
   case e of
     Left err -> App (ExceptT (pure (Left (pretty err))))
     Right x -> pure x
 
-runApp :: App a -> IO a
-runApp (App a) = runStdoutLoggingT $ do
-  e <- runExceptT a
-  case e of
-    Left errDoc -> logErrorN (docToText errDoc) >> liftIO exitFailure
-    Right x -> pure x
+class Extension x => NamedExtension x where
+  extensionName :: String
+
+instance NamedExtension SimpleUnification where
+  extensionName = "simple"
+
+instance NamedExtension TypeClass where
+  extensionName = "class"
+
+data PackedExtension = forall x. NamedExtension x => PackedExtension (Proxy x)
+
+availableExtensions :: [PackedExtension]
+availableExtensions = [PackedExtension (Proxy @SimpleUnification), PackedExtension (Proxy @TypeClass)]
+
+getExtension :: String -> Maybe PackedExtension
+getExtension name = find f availableExtensions
   where
+    f (PackedExtension (Proxy :: Proxy x)) = extensionName @x == name
+
+parseProgram :: Extension x => Text -> App x (Program x)
+parseProgram = handleError . Parser.parseProgram
+
+typeProgram :: Extension x => Program x -> App x ()
+typeProgram = handleError . Type.typeProgram
+
+runApp :: String -> (forall x. Extension x => App x a) -> IO a
+runApp extName app = runStdoutLoggingT $
+  case getExtension extName of
+    Just (PackedExtension (Proxy :: Proxy x)) -> runApp' $ app @x
+    Nothing -> logErrorN (docToText $ "unknown extension" <+> dquotes (pretty extName)) >> liftIO exitFailure
+  where
+    runApp' (App a) = do
+      e <- runExceptT a
+      case e of
+        Left errDoc -> logErrorN (docToText errDoc) >> liftIO exitFailure
+        Right x -> pure x
     docToText = renderStrict . layoutPretty defaultLayoutOptions
--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,18 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
-
 module Main where
 
 import App
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Text.IO as Text (readFile)
 import Language.Simple.Extension (Extension)
-import Language.Simple.Extension.TypeClass (TypeClass)
-import Language.Simple.Parser (parseProgram)
-import Language.Simple.Type (typeProgram)
 import System.Environment (getArgs)
 
-typeFile :: forall x. Extension x => FilePath -> App ()
+typeFile :: Extension x => FilePath -> App x ()
 typeFile file = do
   content <- liftIO $ Text.readFile file
-  program <- handleError $ parseProgram @x content
-  handleError $ typeProgram program
+  program <- parseProgram content
+  typeProgram program
 
 main :: IO ()
 main = do
-  [path] <- getArgs
-  runApp (typeFile @TypeClass path)
+  [ext, path] <- getArgs
+  runApp ext $ typeFile path
