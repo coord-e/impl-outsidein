32eb6f35c6c089a7502108bf3189b23dfca78641
coord.e <me@coord-e.com>
2020-11-03 15:15:44 +0000

    Change: Use resulting unifiers from implication constraints

--- src/Language/Simple/Type/Solver.hs
+++ src/Language/Simple/Type/Solver.hs
@@ -10,6 +10,8 @@ module Language.Simple.Type.Solver (solveConstraint) where
 import Control.Monad (forM_, unless)
 import Control.Monad.Except (MonadError (..))
 import Control.Monad.Logger (MonadLogger)
+import Control.Monad.State (execStateT, get, modify)
+import Control.Monad.Trans (lift)
 import Data.HashSet (HashSet)
 import qualified Data.HashSet as HashSet (toList)
 import Language.Simple.Extension (Extension (..))
@@ -19,6 +21,7 @@ import Language.Simple.Type.Constraint (GeneratedConstraint (..), UniVar, implic
 import Language.Simple.Type.Env (HasProgramEnv)
 import Language.Simple.Type.Error (TypeError (..))
 import Language.Simple.Type.Substitution (Substitutable (..), Unifier)
+import qualified Language.Simple.Type.Substitution as Subst (compose)
 import Language.Simple.Util (logParamsDebug)
 import Prettyprinter (Pretty (..))
 
@@ -34,20 +37,28 @@ solveConstraint ::
   GeneratedConstraint x ->
   m (Constraint x UniVar, Unifier x)
 solveConstraint given tch wanted = do
+  -- solve simple constraints
   (residual, unifier) <- simplifyConstraint given tch (simple wanted)
   let residual' = reduce residual
-  forM_ (implic (substitute unifier wanted)) $ \(vars, premise, c) -> do
-    (q, _) <- solveConstraint (given <> residual' <> premise) vars c
-    unless (isEmpty q) $ throwError (UnresolvedConstraint q)
+
+  -- solve ∃ constraints
+  unifier' <- flip execStateT unifier $
+    forM_ (implic wanted) $ \(vars, premise, c) -> do
+      u <- get
+      (q, u') <- lift $ solveConstraint (given <> residual' <> substitute u premise) vars (substitute u c)
+      unless (isEmpty q) $ throwError (UnresolvedConstraint q)
+      modify (Subst.compose u')
+
   logParamsDebug
     "solveConstraint"
     [ ("given", pretty given),
       ("tch", pretty (HashSet.toList tch)),
       ("wanted", pretty wanted),
       ("(out) residual", pretty residual'),
-      ("(out) unifier", pretty unifier)
+      ("(out) unifier", pretty unifier')
     ]
-  pure (residual', unifier)
+
+  pure (residual', unifier')
   where
     isEmpty EmptyConstraint = True
     isEmpty _ = False
