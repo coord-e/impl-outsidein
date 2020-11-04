46041450ef267a06b85d9648b94f118c530cd2d8
coord.e <me@coord-e.com>
2020-11-04 10:01:20 +0000

    Revert "Change: Use resulting unifiers from implication constraints"
    This reverts commit 32eb6f35c6c089a7502108bf3189b23dfca78641.

--- src/Language/Simple/Type/Solver.hs
+++ src/Language/Simple/Type/Solver.hs
@@ -10,8 +10,6 @@ module Language.Simple.Type.Solver (solveConstraint) where
 import Control.Monad (forM_, unless)
 import Control.Monad.Except (MonadError (..))
 import Control.Monad.Logger (MonadLogger)
-import Control.Monad.State (execStateT, get, modify)
-import Control.Monad.Trans (lift)
 import Data.HashSet (HashSet)
 import qualified Data.HashSet as HashSet (toList)
 import Language.Simple.Extension (Extension (..))
@@ -21,7 +19,6 @@ import Language.Simple.Type.Constraint (GeneratedConstraint (..), UniVar, implic
 import Language.Simple.Type.Env (HasProgramEnv)
 import Language.Simple.Type.Error (TypeError (..))
 import Language.Simple.Type.Substitution (Substitutable (..), Unifier)
-import qualified Language.Simple.Type.Substitution as Subst (compose)
 import Language.Simple.Util (logParamsDebug)
 import Prettyprinter (Pretty (..))
 
@@ -37,28 +34,20 @@ solveConstraint ::
   GeneratedConstraint x ->
   m (Constraint x UniVar, Unifier x)
 solveConstraint given tch wanted = do
-  -- solve simple constraints
   (residual, unifier) <- simplifyConstraint given tch (simple wanted)
   let residual' = reduce residual
-
-  -- solve ∃ constraints
-  unifier' <- flip execStateT unifier $
-    forM_ (implic wanted) $ \(vars, premise, c) -> do
-      u <- get
-      (q, u') <- lift $ solveConstraint (given <> residual' <> substitute u premise) vars (substitute u c)
-      unless (isEmpty q) $ throwError (UnresolvedConstraint q)
-      modify (Subst.compose u')
-
+  forM_ (implic (substitute unifier wanted)) $ \(vars, premise, c) -> do
+    (q, _) <- solveConstraint (given <> residual' <> premise) vars c
+    unless (isEmpty q) $ throwError (UnresolvedConstraint q)
   logParamsDebug
     "solveConstraint"
     [ ("given", pretty given),
       ("tch", pretty (HashSet.toList tch)),
       ("wanted", pretty wanted),
       ("(out) residual", pretty residual'),
-      ("(out) unifier", pretty unifier')
+      ("(out) unifier", pretty unifier)
     ]
-
-  pure (residual', unifier')
+  pure (residual', unifier)
   where
     isEmpty EmptyConstraint = True
     isEmpty _ = False
